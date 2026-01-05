74a1dccb9ed5cd18d9f9eefc6c84d2528b452308
Tasuku Yamashita <task.teckac@gmail.com>
2026-01-05 13:48:37 +0900

    MVP-022: add repo ls

---
+++ internal/repo/list.go
@@ -0,0 +1,62 @@
+package repo
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type Entry struct {
+	RepoKey   string
+	StorePath string
+}
+
+func List(rootDir string) ([]Entry, []error, error) {
+	reposRoot := filepath.Join(rootDir, "repos")
+	info, err := os.Stat(reposRoot)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil, nil
+		}
+		return nil, nil, err
+	}
+	if !info.IsDir() {
+		return nil, nil, fmt.Errorf("repos path is not a directory: %s", reposRoot)
+	}
+
+	var entries []Entry
+	var warnings []error
+
+	err = filepath.WalkDir(reposRoot, func(path string, d os.DirEntry, walkErr error) error {
+		if walkErr != nil {
+			warnings = append(warnings, walkErr)
+			return nil
+		}
+		if !d.IsDir() {
+			return nil
+		}
+		if path == reposRoot {
+			return nil
+		}
+		if filepath.Ext(path) != ".git" {
+			return nil
+		}
+
+		rel, err := filepath.Rel(reposRoot, path)
+		if err != nil {
+			warnings = append(warnings, err)
+			return nil
+		}
+		repoKey := filepath.ToSlash(rel)
+		entries = append(entries, Entry{
+			RepoKey:   repoKey,
+			StorePath: path,
+		})
+		return filepath.SkipDir
+	})
+	if err != nil {
+		return nil, warnings, err
+	}
+
+	return entries, warnings, nil
+}
--- tasks/MVP.md
+++ tasks/MVP.md
@@ -15,7 +15,7 @@
 | MVP-012 | Git command runner | Done |
 | MVP-020 | Repo spec normalization | Done |
 | MVP-021 | gws repo get | Done |
-| MVP-022 | gws repo ls | To Do |
+| MVP-022 | gws repo ls | Done |
 | MVP-030 | gws ws new | To Do |
 | MVP-031 | manifest read/write library | To Do |
 | MVP-032 | gws ws add (worktree add) | To Do |
